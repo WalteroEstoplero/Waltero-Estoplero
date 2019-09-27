026ea26861d3daeffc31a1eff8f437d9d68f87f5
Waltero Estoplero <waltser@web.de>
2019-09-27 18:21:40 +0200

    Program.cs hinzugefügt und test1.txt gelöscht

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHL_Hallo_FH_Vorkurs
+{
+    class Program
+    {
+        static
+void Main(string[]
+args)
+        {
+            Console.
+        WriteLine("Hallo Vorkurs Leibniz-FH!"
+    );
+
+            Console.ReadKey();
+        }
+    }
+}
